a7f284c1aa2afa56eaa69298b36dd6e309904f31
Saiharsha B <34317373+thewhitewolf17@users.noreply.github.com>
2019-03-17 14:38:04 +0530

    Improve Efficiency of Scan and Cscan

--- Disk Scheduling/cscan.py
+++ Disk Scheduling/cscan.py
@@ -6,29 +6,44 @@ hp = int(input())
 pos = hp
 print("Enter the Seek Rate : ")
 srate = int(input())
-print("Enter the Alpha Value : ")
-alpha = int(input())
 print("Enter the Requests : ")
 arr = [ int(input()) for i in range(req)]
 start = 0
-end = 200 #Start and End of Head Positions
+end = 199 #Start and End of Head Positions
 print(hp, end='')
-for i in range(pos, end+1): #Iterates the movement from initial to end of disk
-    if i in arr:
-        thm+= abs(pos-i)
-        pos = i
-        print(" -> ",i, end='')
-        arr.remove(i)
-thm+= abs(pos-end)
-thm+= alpha 
-print(" -> ", end, end='')
-print(" -> ", start, end='')
-for i in range(start, hp+1): #Scans back to start
-    if i in arr:
-        thm+= abs(pos-i)
-        pos = i
-        print(" -> ", i, end='')
-        arr.remove(i)
+if(hp<100):
+    for i in range(pos, start-1, -1):
+        if i in arr:
+            thm+= abs(pos-i)
+            pos = i
+            print(" -> ",i, end='')
+            arr.remove(i)
+    thm+= abs(pos-start)
+    pos = end
+    print(" -> ", start, end='')
+    print(" -> ", end, end='')
+    for i in range(end, hp+1, -1): #Scans back to start
+        if i in arr:
+            thm+= abs(pos-i)
+            pos = i
+            print(" -> ", i, end='')
+            arr.remove(i)
+else:
+    for i in range(pos, end+1):
+        if i in arr: 
+            thm+= abs(pos-i)
+            pos = i
+            print(" -> ", i, end='')
+            arr.remove(i)
+    thm+= abs(pos-end)
+    pos=end
+    print(" -> ", end, end='')
+    for i in range(start,hp+1):
+        if i in arr:
+            time+=abs(pos-i)
+            pos=i
+            print(" -> ",i, end='')
+            arr.remove(i)
 stime = thm * srate
 print("\nThe Total Head Movement is",thm)
 print("The Seek Time is",stime)
--- Disk Scheduling/scan.py
+++ Disk Scheduling/scan.py
@@ -9,23 +9,40 @@ srate = int(input())
 print("Enter the Requests : ")
 arr = [ int(input()) for i in range(req)]
 start = 0
-end = 200 #Start and End of Head Positions
+end = 199 #Start and End of Head Positions
 print(hp, end='')
-for i in range(pos, end+1): #Iterates the movement from initial to end of disk
-    if i in arr:
-        thm+= abs(pos-i)
-        pos = i
-        print(" -> ",i, end='')
-        arr.remove(i)
-thm+= abs(pos-end)
-pos = end 
-print(" -> ", end, end='')
-for i in range(pos, start-1,-1): #Scans back to start
-    if i in arr:
-        thm+= abs(pos-i)
-        pos = i
-        print(" -> ", i, end='')
-        arr.remove(i)
+if(hp<100):
+    for i in range(pos, start-1, -1): #Iterates the movement from initial to end of disk
+        if i in arr:
+            thm+= abs(pos-i)
+            pos = i
+            print(" -> ",i, end='')
+            arr.remove(i)
+    thm+= abs(pos-start)
+    pos = start 
+    print(" -> ", start, end='')
+    for i in range(pos, end+1): #Scans back to start
+        if i in arr:
+            thm+= abs(pos-i)
+            pos = i
+            print(" -> ", i, end='')
+            arr.remove(i)
+else:
+    for i in range(pos, end+1): #Iterates the movement from initial to end of disk
+        if i in arr:
+            thm+= abs(pos-i)
+            pos = i
+            print(" -> ",i, end='')
+            arr.remove(i)
+    thm+= abs(pos-end)
+    pos = end 
+    print(" -> ", end, end='')
+    for i in range(pos, start-1,-1): #Scans back to start
+        if i in arr:
+            thm+= abs(pos-i)
+            pos = i
+            print(" -> ", i, end='')
+            arr.remove(i)
 stime = thm * srate
 print("\nThe Total Head Movement is",thm)
 print("The Seek Time is",stime)
