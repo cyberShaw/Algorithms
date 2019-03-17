925a7c546198af3297d739390dd633f42061f62b
Saiharsha B <34317373+thewhitewolf17@users.noreply.github.com>
2019-03-17 13:54:01 +0530

    Add CScan Disk Scheduling Algorithm

--- Disk Scheduling/cscan.py
+++ Disk Scheduling/cscan.py
@@ -6,6 +6,8 @@ hp = int(input())
 pos = hp
 print("Enter the Seek Rate : ")
 srate = int(input())
+print("Enter the Alpha Value : ")
+alpha = int(input())
 print("Enter the Requests : ")
 arr = [ int(input()) for i in range(req)]
 start = 0
@@ -18,9 +20,10 @@ for i in range(pos, end+1): #Iterates the movement from initial to end of disk
         print(" -> ",i, end='')
         arr.remove(i)
 thm+= abs(pos-end)
-pos = end 
+thm+= alpha 
 print(" -> ", end, end='')
-for i in range(pos, start-1,-1): #Scans back to start
+print(" -> ", start, end='')
+for i in range(start, hp+1): #Scans back to start
     if i in arr:
         thm+= abs(pos-i)
         pos = i
