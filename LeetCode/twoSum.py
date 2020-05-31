1dc1bbb37c38c5b131ac962e1610563bfb9d6f29
Saiharsha B <saibalsu@gmail.com>
2020-05-31 20:38:01 +0530

    LeetCode Interview Problems - Easy

--- LeetCode/twoSum.py
+++ LeetCode/twoSum.py
@@ -9,4 +9,10 @@ class Solution:
                 if(nums[i]+nums[j]==target):
                     return i, j
 
-
+# O(n) Solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = { v:k for k, v in enumerate(nums) }
+        for i in range(len(nums)):
+            if target - nums[i] in dict and nums.index(target - nums[i]) != i:
+                return i, nums.index(target-nums[i])
