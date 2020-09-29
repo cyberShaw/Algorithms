7f12a805e523e2e8fbcbd4151565086c95997db3
Saiharsha B <saibalsu@gmail.com>
2020-09-30 02:20:14 +0530

    add CC DSA Learning Series solutions

--- CodeChef/DSA Learning Series/Contest 1/Carvans.py
+++ CodeChef/DSA Learning Series/Contest 1/Carvans.py
@@ -1,11 +1,15 @@
-import sys
-T = int(sys.stdin.readline())
-while T:
-    n = int(sys.stdin.readline())
-    speeds = list(map(int, sys.stdin.readline().split()))
-    top = 1
-    for i in range(1, n):
-        if speeds[i] <= speeds[i-1]:
-            top += 1
-    print(top)
-    T -= 1
+def solve():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        A = list(map(int,input().split()))
+        count = 0
+        small = float("inf")
+        for each in A:
+            if each < small:
+                small = each
+                count += 1
+        print(count)
+        
+if __name__ == "__main__":
+    solve()
